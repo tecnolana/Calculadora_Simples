9a8154a54bc67b66969e4d5cef89d8637a199bfd
tecnolana <eloisioti@gmail.com>
2023-10-02 20:29:33 -0300

    Calculadora_Simples

---
+++ script.js
@@ -0,0 +1,13 @@
+function displaynum(n1) {
+    Calculator.text1.value = Calculator.text1.value + n1;
+
+}
+
+/*function calcular() {
+    document.getElementById("formulario");
+    var raiz = formulario.valor.value;
+    raiz = (Math.sqrt(raiz));
+    formulario.resultado.value = resultado;
+}*/
+
+
